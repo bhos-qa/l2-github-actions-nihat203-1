f9abffa09cac67b5d25afc00beb02db0e75f0f8b
nihat203 <nihat.aliyev.std@bhos.edu.az>
2023-09-24 21:15:23 +0400

    Add Gradle project for feature/lab2

---
+++ src/test/java/MyClassTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyClassTest {
+    @Test
+    public void testAdd() {
+        MyClass myClass = new MyClass();
+        int result = myClass.add(2, 3);
+
+        assertEquals(5, result);
+    }
+}
